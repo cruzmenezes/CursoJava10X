a600a98da53d46ba6322b798c72aad279b34f347
cruzmenezes <cruz.menezes@outlook.com>
2025-07-03 11:20:00 -0300

    exemplo while e Switch case

---
+++ CursoJava10XProjetos/cursoJavaProjetos/src/ExemploWhileForDoWhile/ExemploWhile.java
@@ -0,0 +1,13 @@
+package ExemploWhileForDoWhile;
+
+public class ExemploWhile {
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        while (contador < 5){
+            System.out.println("contador " + contador);
+            contador ++;
+        }
+    }
+}
