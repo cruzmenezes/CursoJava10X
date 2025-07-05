aff3ff08126e1d95057c2669f576b7a3f3fc6f34
cruzmenezes <cruz.menezes@outlook.com>
2025-07-05 16:28:25 -0300

    exemplo for

---
+++ CursoJava10XProjetos/cursoJavaProjetos/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        int valor = 0;
+        for (valor = 0; valor < 5; valor ++){
+            System.out.println("valor: " + valor);
+        }
+    }
+}
