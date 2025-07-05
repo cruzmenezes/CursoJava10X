f29b49900b27eb27900d35a25b39de320f015efb
cruzmenezes <cruz.menezes@outlook.com>
2025-07-05 09:59:58 -0300

    exemplo if else

---
+++ CursoJava10XProjetos/cursoJavaProjetos/src/ExemploIfElse/ExemploIfElse.java
@@ -0,0 +1,14 @@
+package ExemploIfElse;
+
+public class ExemploIfElse {
+    public static void main(String[] args) {
+
+        int valor1 = 30;
+
+        if (valor1 > 20 ){
+            System.out.println("valor maior");
+        }else{
+            System.out.println("valor menor");
+        }
+    }
+}
