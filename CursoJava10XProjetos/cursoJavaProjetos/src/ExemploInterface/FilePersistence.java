ec03ed5316d9ea557277eb1ef93d5fa1505b37fc
cruzmenezes <cruz.menezes@outlook.com>
2025-07-10 21:54:53 -0300

    exercio dio

---
+++ CursoJava10XProjetos/cursoJavaProjetos/src/ExemploInterface/FilePersistence.java
@@ -0,0 +1,14 @@
+package ExemploInterface;
+
+public interface FilePersistence {
+
+    String write(final  String data);
+
+    boolean remove(final String sentence);
+
+    String replace (final String oldContent, final String newContent);
+
+    String findAll();
+
+    String findBy(final String sentence);
+}
