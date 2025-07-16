3409866162974573f5bf47d6a571166a88e14cce
cruzmenezes <cruz.menezes@outlook.com>
2025-07-15 21:30:32 -0300

    exemplos de classes e objetos

---
+++ CursoJava10XProjetos/cursoJavaProjetos/src/ExemplosClass/PlantaCasa.java
@@ -0,0 +1,22 @@
+package ExemplosClass;
+
+public class PlantaCasa {
+    int metragem;
+    int numeroQuarto;
+    int numeroBanheiro;
+    String corCasa;
+    String material;
+
+    public void construir(){
+        System.out.println("metragem: " + metragem);
+        System.out.println("Numero de quartos: " + numeroQuarto);
+        System.out.println("Numero de banheiro: " + numeroBanheiro);
+        System.out.println("Material utilizado: " + material);
+    }
+
+
+    public void pintar(){
+        System.out.println("Cor da casa: " + corCasa);
+
+    }
+}
---
+++ CursoJava10XProjetos/cursoJavaProjetos/src/ExemplosClass/PrincipalCasa.java
@@ -0,0 +1,19 @@
+package ExemplosClass;
+
+public class PrincipalCasa {
+    public static void main(String[] args) {
+        PlantaCasa casa = new PlantaCasa();
+
+        System.out.println("== as caracterisca da sua casa ==");
+        casa.metragem = 1000;
+        casa.numeroQuarto = 4;
+        casa.numeroBanheiro = 4;
+        casa.material = "tijolo ecologico";
+        casa.corCasa = "branco";
+
+        casa.construir();
+        casa.pintar();
+
+
+    }
+}
